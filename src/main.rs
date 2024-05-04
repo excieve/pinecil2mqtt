9e4ec418b3e4447f04d5ab2e29f00e9c5f9f677e
Artem Hluvchynskyi <excieve@gmail.com>
2024-05-04 21:08:22 +0300

    refactor(pinecil): move Pinecil management to a trait + struct

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,72 @@
 use log::info;
 use futures::stream::StreamExt;
+use std::error::Error;
 
 use btleplug::api::{Central, Manager as _, ScanFilter, CentralEvent, Peripheral};
-use btleplug::platform::Manager;
+use btleplug::platform::{Manager, Adapter};
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+async fn main() -> Result<(), Box<dyn Error>> {
     env_logger::init();
 
     info!("started!");
 
-    // Initialise the btleplug manager and central
-    let manager = Manager::new().await?;
-
-    let adapter_list = manager.adapters().await?;
-    if adapter_list.is_empty() {
-        return Err("No Bluetooth adapters found".into());
-    }
-
-    let central = adapter_list.into_iter().nth(0).unwrap();
-
-    // Start the event loop and scan for devices
-    let mut events = central.events().await?;
-
-    central.start_scan(ScanFilter::default()).await?;
-
-    while let Some(event) = events.next().await {
-        match event {
-            CentralEvent::DeviceDiscovered(addr) => {
-                let device = central.peripheral(&addr).await?;
-                info!("Discovered device: {:?}", device.properties().await?);
-            }
-            CentralEvent::DeviceConnected(addr) => {
-                info!("Device connected: {:?}", addr);
-            }
-            CentralEvent::DeviceDisconnected(addr) => {
-                info!("Device disconnected: {:?}", addr);
-            }
-            _ => {}
-        }
-    }
+    let manager = PinecilManagerBtle::new().await?;
+    manager.process_events().await?;
 
     Ok(())
+}
+
+trait PinecilManager {
+    async fn process_events(&self) -> Result<(), Box<dyn Error>>;
+}
+
+// PinecilManagerBtle encapsulates the Pinecil BLE peripherals though the btleplug Manager API
+struct PinecilManagerBtle {
+    manager: Manager,
+}
+
+impl PinecilManagerBtle {
+    async fn new() -> Result<Self, Box<dyn Error>> {
+        let manager = Manager::new().await?;
+
+        Ok(Self { manager })
+    }
+
+    async fn get_first_adapter(&self) -> Result<Adapter, Box<dyn Error>> {
+        let adapter_list = self.manager.adapters().await?;
+        if adapter_list.is_empty() {
+            return Err("No adapters found".into());
+        }
+
+        Ok(adapter_list.into_iter().next().unwrap())
+    }
+}
+
+impl PinecilManager for PinecilManagerBtle {
+    // Process the events from the adapter continuously
+    async fn process_events(&self) -> Result<(), Box<dyn Error>> {
+        let central = self.get_first_adapter().await?;
+        central.start_scan(ScanFilter::default()).await?;
+
+        let mut events = central.events().await?;
+
+        while let Some(event) = events.next().await {
+            match event {
+                CentralEvent::DeviceDiscovered(addr) => {
+                    let device = central.peripheral(&addr).await?;
+                    info!("Discovered device: {:?}", device.properties().await?);
+                }
+                CentralEvent::DeviceConnected(addr) => {
+                    info!("Device connected: {:?}", addr);
+                }
+                CentralEvent::DeviceDisconnected(addr) => {
+                    info!("Device disconnected: {:?}", addr);
+                }
+                _ => {}
+            }
+        }
+
+        Ok(())
+    }
 }
