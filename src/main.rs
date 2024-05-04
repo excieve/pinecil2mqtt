607cd71c2d08f040aba2b0736296cd1857884f7d
Artem Hluvchynskyi <excieve@gmail.com>
2024-05-05 00:24:41 +0300

    feat(pinecil): Pinecil bulk BLE characteristics query stub

--- src/main.rs
+++ src/main.rs
@@ -122,3 +122,45 @@ impl PinecilManager for PinecilManagerBtle {
         Ok(())
     }
 }
+
+struct PinecilBulkData {
+    live_temp: u32,
+    set_point: u32,
+    voltage: u32,
+    handle_temp: u32,
+    power_level: u32,
+    power_source: u32,
+    tip_resistance: u32,
+    uptime: u32,
+    last_movement: u32,
+    max_tip_temp: u32,
+    tip_voltage: u32,
+    hall_sensor: u32,
+    operating_mode: u32,
+    power: u32,
+}
+
+trait PinecilBulkQuery {
+    async fn query_pinecil_info(&self) -> Result<String, Box<dyn Error>>;
+    async fn query_bulk_data(&self) -> Result<PinecilBulkData, Box<dyn Error>>;
+}
+
+struct PinecilBulkQueryBtle {
+    device: Peripheral,
+}
+
+impl PinecilBulkQueryBtle {
+    fn new(device: Peripheral) -> Self {
+        Self { device }
+    }
+}
+
+impl PinecilBulkQuery for PinecilBulkQueryBtle {
+    async fn query_pinecil_info(&self) -> Result<String, Box<dyn Error>> {
+        todo!()
+    }
+
+    async fn query_bulk_data(&self) -> Result<PinecilBulkData, Box<dyn Error>> {
+        todo!()
+    }
+}
